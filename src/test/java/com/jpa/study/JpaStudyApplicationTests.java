77296fb22ec81f846ee03992c6111aff85b7efea
leedonghu <ldh930517@gmail.com>
2022-07-07 22:36:22 +0900

    jpa study

--- src/test/java/com/jpa/study/JpaStudyApplicationTests.java
+++ src/test/java/com/jpa/study/JpaStudyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.jpa.study;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.MethodOrderer;
@@ -10,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.jpa.study.entity.User;
+import com.jpa.study.entity.User02;
 import com.jpa.study.repository.UserRepository;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -62,5 +65,88 @@ class JpaStudyApplicationTests {
 			log.info("findAll : " + u.getID() + " | " + u.getUsername());
 		}
 	}
+	
+	
+//	@Test
+//	@Order(1)
+//	void save() {
+//		User02 user = new User02();
+//		user.setFirstName("qwe");
+//		user.setLastName("asd");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("tty");
+//		user.setLastName("ppo");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("abc");
+//		user.setLastName("def");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("ghi");
+//		user.setLastName("jkl");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("mno");
+//		user.setLastName("pqr");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("stu");
+//		user.setLastName("vwx");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("stu");
+//		user.setLastName("123");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("STu");
+//		user.setLastName("124");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("STU");
+//		user.setLastName("ABD");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("yza");
+//		user.setLastName("bcd");
+//		userRepository.save(user);
+//		
+//		user.setFirstName("efg");
+//		user.setLastName("hij");
+//		userRepository.save(user);
+//	}
+//	
+//	@Test
+//	@Order(2)
+//	void select() {
+//		List<User02> userList = userRepository.findFirst5ByLastNameAndFirstNameOrderByUserIdDesc("vwx", "stu");
+//		for(User02 u : userList) {
+//			log.info(u.getFirstName() + " | " + u.getLastName());
+//		}
+//	}
+//	
+//	@Test
+//	@Order(3)
+//	void select02() {
+//		List<User02> userList = userRepository.findAll();
+//		for(User02 u : userList) {
+//			log.info("findAll : "+u.getFirstName() + " | " + u.getLastName());
+//		}
+//	}
+//	
+//	@Test
+//	@Order(4)
+//	void select03() {
+//		boolean check = userRepository.existsByStartDateLessThanEqual(LocalDateTime.now());
+//		log.info(String.valueOf(check));
+//	}
+//	
+//	@Test
+//	@Order(5)
+//	void select04() {
+//		long cnt = userRepository.conutByFirstNameIgnoreCaseLike("stu%");
+//		log.info(String.valueOf(cnt));
+//		
+//	}
 
 }
